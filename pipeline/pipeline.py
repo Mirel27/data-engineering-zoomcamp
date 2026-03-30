38023e82f57a48e126415d8cf6854d82df85063a
Mirel27 <148498431+Mirel27@users.noreply.github.com>
2026-03-30 14:29:38 +0000

    Dockers

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+
+print("arguments", sys.argv) # to pass arguments
+month = sys.argv[1] # get the first argument
+print("month", month) # print the month argument
+
+df=pd.DataFrame({"A":[1,2],"B":[3,4]})
+print(df.head()) # print the first 5 rows of the dataframe
+df.to_parquet(f"output/{month}.parquet") # save the dataframe as a parquet file in the output directory with the name of the month
